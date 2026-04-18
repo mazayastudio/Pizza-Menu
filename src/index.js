8f6a33e73f731bdee067b7393d017fc7cd0a7cbc
sayudhalw <mazaya.studio@outlook.com>
2026-04-19 06:03:35 +0700

    # 42. Passing and Receiving Props
    Refactor Pizza component for dynamic rendering:
    
    Update `Pizza` component to accept props for customization and streamline `Menu` component structure.
    
    Signed-off-by: sayudhalw <mazaya.studio@outlook.com>

--- src/index.js
+++ src/index.js
@@ -57,15 +57,6 @@ function App () {
   )
 }
 
-function Pizza () {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci"/>
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  )
-}
 
 function Header () {
   return (
@@ -81,11 +72,26 @@ function Menu () {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza/>
-      <Pizza/>
-      <Pizza/>
-      <Pizza/>
+      <Pizza
+        name='Pizza Spinaci'
+        ingredients='Tomato, mozarella, spinach, and ricotta cheese'
+        price={12}
+        photoName='pizzas/spinaci.jpg'
+      />
     </main>
+  )
+}
+
+function Pizza (props) {
+  return (
+    <div className='pizza'>
+      <img src={props.photoName} alt={props.name}/>
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>${props.price}</span>
+      </div>
+    </div>
   )
 }
 
