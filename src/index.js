042aec7a3183edaf5e642ed891bfbcda27e8d219
sayudhalw <mazaya.studio@outlook.com>
2026-04-18 23:16:47 +0700

    # 41. Styling React Applications
    Enhance UI styles and structure:
    
    Add CSS file and class names for layout improvements
    
    Signed-off-by: sayudhalw <mazaya.studio@outlook.com>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css'
 
 const pizzaData = [
   {
@@ -48,7 +49,7 @@ const pizzaData = [
 
 function App () {
   return (
-    <div>
+    <div className="container">
       <Header/>
       <Menu/>
       <Footer/>
@@ -60,31 +61,43 @@ function Pizza () {
   return (
     <div>
       <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci"/>
-      <h2>Pizza Spinaci</h2>
+      <h3>Pizza Spinaci</h3>
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
   )
 }
 
 function Header () {
-  return <h1>Fast React Pizza Co.</h1>
+  return (
+    <header className="header">
+      <h1>
+        Fast React Pizza Co.
+      </h1>
+    </header>
+  )
 }
 
 function Menu () {
   return (
-    <div>
+    <main className="menu">
       <h2>Our Menu</h2>
       <Pizza/>
       <Pizza/>
       <Pizza/>
       <Pizza/>
-    </div>
+    </main>
   )
 }
 
 function Footer () {
+  const hour = new Date().getHours()
+  const openHour = 12
+  const closeHour = 22
+  const isOpen = hour >= openHour && hour <= closeHour
+
+
   return (
-    <footer>
+    <footer className="footer">
       <p>{new Date().toLocaleTimeString()}. We're currently open!</p>
       <p>© 2023 Fast React Pizza Co. All rights reserved.</p>
     </footer>
